af37faedb8d2f1482018522ae10046c8f574c052
rikhtaa <rekhta674@gmail.com>
2025-06-19 17:23:19 +0500

    add lint staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,9 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
    tseslint.configs.recommendedTypeChecked,
-   {ignores:['dist', 'node_modules']},
+   {
+    ignores:['dist', 'node_modules', 'eslint.config.mjs']
+   },
   {
     languageOptions: {
       parserOptions: {
