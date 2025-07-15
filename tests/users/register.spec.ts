75eca00777c42bcb9f89bc1be24b3bfabbdc962b
Rekhta Menahil <rekhta674@gmail.com>
2025-07-15 20:46:16 +0500

    email require validation

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -5,23 +5,22 @@ import { AppDataSource } from '../../src/config/data-source'
 import { User } from '../../src/entity/User'
 import { Roles } from '../../src/constants'
 describe('POST /auth/register', () => {
+    let connection: DataSource
+    //to connect the connection to Db jest hooks can be used
+    //beforeAll will run before all tests
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize()
+    })
+
+    beforeEach(async () => {
+        await connection.dropDatabase()
+        await connection.synchronize()
+    })
+
+    afterAll(async () => {
+        await connection.destroy()
+    })
     describe('Given all fields', () => {
-        let connection: DataSource
-        //to connect the connection to Db jest hooks can be used
-        //beforeAll will run before all tests
-        beforeAll(async () => {
-            connection = await AppDataSource.initialize()
-        })
-
-        beforeEach(async () => {
-            await connection.dropDatabase()
-            await connection.synchronize()
-        })
-
-        afterAll(async () => {
-            await connection.destroy()
-        })
-
         it('should return the 201 status code', async () => {
             //AAA
             //Arrange
@@ -153,5 +152,26 @@ describe('POST /auth/register', () => {
             expect(users).toHaveLength(1)
         })
     })
-    describe('Fields are  missing', () => {})
+    describe('Fields are  missing', () => {
+        it('should return 400 status code if email field is missing', async () => {
+            //Arrange
+            const userData = {
+                firstName: 'Rikhta',
+                lastName: 'K',
+                email: '',
+                password: 'secret',
+            }
+
+            //Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            //Assert
+            expect(response.statusCode).toBe(400)
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+            expect(users).toHaveLength(0)
+        })
+    })
 })
