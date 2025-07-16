de48480a452fbb0143eac5debaf74e639832996d
Rekhta Menahil <rekhta674@gmail.com>
2025-07-16 20:04:57 +0500

    test token generation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ export default tseslint.config(
             'node_modules',
             'eslint.config.mjs',
             'jest.config.js',
+            'scripts/generateKeys.mjs',
         ],
     },
     {
