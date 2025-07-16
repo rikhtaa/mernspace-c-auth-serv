d20f02d5da1a862d5c42966300fb92fe61cd0eea
Rekhta Menahil <rekhta674@gmail.com>
2025-07-16 23:15:31 +0500

    persist  refresh token

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -5,6 +5,7 @@ import { AppDataSource } from '../../src/config/data-source'
 import { User } from '../../src/entity/User'
 import { Roles } from '../../src/constants'
 import { isJwt } from '../utils'
+import { RefreshToken } from '../../src/entity/RefreshToken'
 describe('POST /auth/register', () => {
     let connection: DataSource
     //to connect the connection to Db jest hooks can be used
@@ -187,6 +188,32 @@ describe('POST /auth/register', () => {
             console.log(accessToken)
             expect(isJwt(accessToken)).toBeTruthy()
             expect(isJwt(refreshToken)).toBeTruthy()
+        })
+        it('should store the refresh token in the database', async () => {
+            //Arrange
+            const userData = {
+                firstName: 'rekhta',
+                lastName: 'Menahil',
+                email: 'rekhta@gmail.com',
+                password: 'secretPassword',
+            }
+
+            //Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            //Assert
+            const refreshTokenRepo = connection.getRepository(RefreshToken)
+            // const refreshTokens = await refreshTokenRepo.find()
+
+            const tokens = await refreshTokenRepo
+                .createQueryBuilder('refreshToken')
+                .where('refreshToken.userId = :userId', {
+                    userId: (response.body as Record<string, string>).id,
+                })
+                .getMany()
+            expect(tokens).toHaveLength(1)
         })
     })
     describe('Fields are  missing', () => {
