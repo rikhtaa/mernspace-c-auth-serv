ae2b6c28c06beaca27271f416a38f9f8e9fd017d
Rekhta Menahil <rekhta674@gmail.com>
2025-07-17 13:36:30 +0500

    change status code of  login endpoint

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -146,7 +146,7 @@ export class AuthController {
                 httpOnly: true, //important
             })
             this.logger.info('user has been logged in', { id: user.id })
-            res.status(201).json({ id: user.id })
+            res.json({ id: user.id })
         } catch (err) {
             next(err)
             return
