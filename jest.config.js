3d8fff277bc1f5d91aa4c1c13196a20d1b03f3e4
Rekhta Menahil <rekhta674@gmail.com>
2025-07-23 14:48:34 +0500

    add test coverage config in package.json and jest.config

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,15 @@ const tsJestTransformCfg = createDefaultPreset().transform
 
 /** @type {import("jest").Config} **/
 module.exports = {
+    preset: "ts-jest",
     testEnvironment: 'node',
-    transform: {
-        ...tsJestTransformCfg,
-    },
     verbose: true,
+    collectCoverage: true,
+    coverageProvider: "v8",
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!tests/**",
+        "!**/node_modules/**"
+    ]
+
 }
