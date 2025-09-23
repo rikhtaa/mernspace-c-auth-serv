8b4b5ccfec27891c77cce0de3a11e25b1da68f40
Rekhta Menahil <rekhta674@gmail.com>
2025-09-23 21:06:29 +0500

    remove raw data from tenant controller

--- src/controllers/TenantController.ts
+++ src/controllers/TenantController.ts
@@ -58,7 +58,6 @@ export class TenantController {
                 total: count,
                 data: tenants,
             })
-            res.json(tenants)
         } catch (err) {
             next(err)
         }
